f6f1871742beab5911e5340b23919375913e033d
Saydum Khalibekov <saydum.it5@gmail.com>
2023-04-26 15:11:05 +0300

    feat: Added new command

--- src/main/java/ru/saydum/Main.java
+++ src/main/java/ru/saydum/Main.java
@@ -1,16 +1,60 @@
 package ru.saydum;
 
-import ru.saydum.VoiceCommand;
+import edu.cmu.sphinx.api.Configuration;
+import edu.cmu.sphinx.api.LiveSpeechRecognizer;
+import edu.cmu.sphinx.api.SpeechResult;
 
-import java.util.ArrayList;
+import java.io.IOException;
 
 public class Main {
-    //declaring voice
     public static void main(String[] args) {
-        VoiceCommand voiceCommand = new VoiceCommand();
 
-        voiceCommand.run("Open chrome", "google-chrome", "Yes sir");
-        voiceCommand.run("Open youtube", "google-chrome youtube.com", "Of course sir");
-        voiceCommand.run("Open calculator", "gnome-calculator", "Yes sir");
+        Configuration config = new Configuration();
+
+        config.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");//hard coded path
+        config.setDictionaryPath("src/main/resources/5214.dic");//need to make file from sphinx knowledge base convertor
+        config.setLanguageModelPath("src/main/resources/5214.lm");
+
+        try {
+            //making system understand you are speaking
+            LiveSpeechRecognizer speech = new LiveSpeechRecognizer(config);
+            speech.startRecognition(true);
+
+            SpeechResult speechResult = null;
+            System.out.println("listening");
+            //running until user is speaking
+            while ((speechResult = speech.getResult()) != null) {
+                String voiceCommandSp = speechResult.getHypothesis();
+                System.out.println("Synthesizer Command is " + voiceCommandSp);
+
+                if (voiceCommandSp.equalsIgnoreCase("Open chrome")) {
+                    Runtime.getRuntime().exec("google-chrome");
+                }
+
+                if (voiceCommandSp.equalsIgnoreCase("Open youtube")) {
+                    Runtime.getRuntime().exec("google-chrome youtube.com");
+                }
+
+                if (voiceCommandSp.equalsIgnoreCase("Open calculator")) {
+                    Runtime.getRuntime().exec("gnome-calculator");
+                }
+
+                if (voiceCommandSp.equalsIgnoreCase("Open project")) {
+//                    Runtime.getRuntime().exec("");
+                    Runtime.getRuntime().exec("code .");
+                }
+                if (voiceCommandSp.equalsIgnoreCase("Play music")) {
+//                    Runtime.getRuntime().exec("");
+                    Runtime.getRuntime().exec("mplayer lofi.mp3");
+                }
+//
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (RuntimeException re) {
+            throw new RuntimeException(re);
+        }
     }
 }
